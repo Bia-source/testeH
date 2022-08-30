89e260381fe41f51595f045d7870b84cf485b50a
Ferreirinha <60610706+Bia-source@users.noreply.github.com>
2022-08-29 22:58:47 -0300

    Update tarefas.routes.ts

--- routes/tarefas.routes.ts
+++ routes/tarefas.routes.ts
@@ -3,20 +3,20 @@ import { CriandoTarefasController } from "../controllers/criandoTarefasControlle
 import { DeletandoTarefasController } from "../controllers/deletandoTarefasController";
 import { PegandoTarefasByPkController } from "../controllers/pegandoTarefasByPkController";
 import { PegandoTarefasController } from "../controllers/pegandoTarefasController";
-import { UpdateTarefasController } from "../controllers/updateTarefasController";
+//import { UpdateTarefasController } from "../controllers/updateTarefasController";
 
 const routerTarefas = Router();
 
 const criandoTarefa = new CriandoTarefasController();
 const pegandoTarefas = new PegandoTarefasController();
-const updateTarefa = new UpdateTarefasController();
+//const updateTarefa = new UpdateTarefasController();
 const deletandoTarefas = new DeletandoTarefasController();
 const pegandoTarefaByPk = new PegandoTarefasByPkController();
 
 routerTarefas.post('/tarefas', criandoTarefa.handle);
 routerTarefas.get('/tarefas', pegandoTarefas.handle);
 routerTarefas.get('/tarefas/:id', pegandoTarefaByPk.handle);
-routerTarefas.put('/tarefas/:id', updateTarefa.handle);
+//routerTarefas.put('/tarefas/:id', updateTarefa.handle);
 routerTarefas.delete('/tarefas/:id', deletandoTarefas.handle);
 
 export { routerTarefas }
